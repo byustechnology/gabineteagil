4acde5e284a7cbf56f2a7c7da66d29c9cfc3f743
Pedro Roccon <pedro@pedroroccon.com.br>
2021-08-07 01:05:56 -0300

    Ajuste de layout nas mensagens de erro

--- byustechnology/gabineteagil/src/views/errors/list.blade.php
+++ byustechnology/gabineteagil/src/views/errors/list.blade.php
@@ -3,16 +3,12 @@
 <div class="modal fade" id="m-app-errors" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content shadow-lg border-0">
-            <div class="modal-body bg-danger text-white">
+            <div class="modal-body bg-danger text-white rounded-lg">
                 <ul class="list-unstyled">
                     @foreach($errors->all() as $error)
                         <li>{{ $error }}</li>
                     @endforeach
                 </ul>
-                
-            </div>
-            <div class="modal-footer bg-danger text-white" style="border-top: rgba(0, 0, 0, .2) 1px solid;">
-                <button type="submit" class="btn btn-light" data-dismiss="modal">Confirmar</button>
             </div>
         </div>
     </div>
