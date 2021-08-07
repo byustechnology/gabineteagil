6857a77353d7b76b2b702011ffe3bb13fbd995ff
Pedro Roccon <pedro@pedroroccon.com.br>
2021-08-07 11:02:15 -0300

    Corrigindo erro de padding e margin

--- byustechnology/gabineteagil/src/views/errors/list.blade.php
+++ byustechnology/gabineteagil/src/views/errors/list.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content shadow-lg border-0">
             <div class="modal-body bg-danger text-white rounded-lg">
-                <ul class="list-unstyled">
+                <ul class="list-unstyled mb-0">
                     @foreach($errors->all() as $error)
                         <li>{{ $error }}</li>
                     @endforeach
