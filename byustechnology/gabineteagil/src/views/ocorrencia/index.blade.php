853377e9ed4d2e1fab499f662fbd12b65e95dcd6
Pedro Roccon <pedro@pedroroccon.com.br>
2021-08-10 22:50:49 -0300

    Ocultando aviso de avançar etapa em etapas concluídas

--- byustechnology/gabineteagil/src/views/ocorrencia/index.blade.php
+++ byustechnology/gabineteagil/src/views/ocorrencia/index.blade.php
@@ -63,7 +63,7 @@
                             </td>
                             <td class="text-center">
                                 <span class="badge py-1 animate__animated animate__flash animate__infinite" style="background: {{ $ocorrencia->etapa->cor }}; color: {{ $ocorrencia->etapa->cor_texto }}">{{ $ocorrencia->etapa->titulo }}</span><br>
-                                @if ( ! $ocorrencia->ultima_etapa)
+                                @if ( ! $ocorrencia->ultima_etapa and ! $ocorrencia->concluida())
                                     <a href="#" data-toggle="modal" data-target="#m-avancar-listagem" data-url="{{ route('ocorrencia.etapa.avancar', ['ocorrencia' => $ocorrencia]) }}" data-protocolo="{{ $ocorrencia->protocolo }}"><small>Avançar etapa <i class="fas fa-arrow-right fa-fw"></i></a></a>
                                 @endif
                             </td>
