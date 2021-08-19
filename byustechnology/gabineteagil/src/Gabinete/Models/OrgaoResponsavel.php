29b443030bb3abe41b934677f6214d7e945b80d5
Pedro Roccon <pedro@pedroroccon.com.br>
2021-08-18 21:08:25 -0300

    Ajuste no modelo referente ao orgão responsável

--- byustechnology/gabineteagil/src/Gabinete/Models/OrgaoResponsavel.php
+++ byustechnology/gabineteagil/src/Gabinete/Models/OrgaoResponsavel.php
@@ -11,66 +11,66 @@ use Illuminate\Database\Eloquent\Model;
 class OrgaoResponsavel extends Model
 {
     use HasFactory, HasFilters, HasColorFields, NeedsAutocode;
-
+    
     /**
-     * Define o nome da tabela relacionado 
-     * a este recurso.
-     * 
-     * @var string
-     */
+    * Define o nome da tabela relacionado 
+    * a este recurso.
+    * 
+    * @var string
+    */
     protected $table = 'orgaos_responsaveis';
-
+    
     /**
-     * Definindo os campos que podem 
-     * ser preenchidos.
-     *
-     * @var array
-     */
-     protected $fillable = [
-         'codigo', 
-         'titulo', 
-         'descricao', 
-         'cor', 
-     ];
-
-     /**
-     * Define as contagens que precisam 
-     * ser carregadas com este 
-     * modelo.
-     * 
-     * @var array
-     */
+    * Definindo os campos que podem 
+    * ser preenchidos.
+    *
+    * @var array
+    */
+    protected $fillable = [
+        'codigo', 
+        'titulo', 
+        'descricao', 
+        'cor', 
+    ];
+    
+    /**
+    * Define as contagens que precisam 
+    * ser carregadas com este 
+    * modelo.
+    * 
+    * @var array
+    */
     protected $withCount = [
         'ocorrencias', 
     ];
-
-     /**
-      * Um orgão pode possuir várias 
-      * ocorrências associadas a ele.
-      *
-      * @return \Illuminate\Database\Eloquent\Collection
-      */
-     public function ocorrencias()
-     {
-        return $this->hasMany(Ocorrencia::class);
-     }
     
     /**
-     * Define um caminho para o modelo.
-     *
-     * @return string
-     */
+    * Um orgão pode possuir várias 
+    * ocorrências associadas a ele.
+    *
+    * @return \Illuminate\Database\Eloquent\Collection
+    */
+    public function ocorrencias()
+    {
+        return $this->hasMany(Ocorrencia::class);
+    }
+    
+    /**
+    * Define um caminho para o modelo.
+    *
+    * @return string
+    */
     public function path()
     {
         return config('gabinete.path') . '/orgao/' . $this->id;
     }
-
+    
     /**
-     * Scope responsável pela ordenação do recurso.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
+    * Scope responsável pela ordenação do recurso.
+    *
+    * @param  \Illuminate\Database\Eloquent\Builder  $query
+    * @return \Illuminate\Database\Eloquent\Builder
+    */
     public function scopeOrdenado($query)
     {
         return $query->orderBy('titulo');
