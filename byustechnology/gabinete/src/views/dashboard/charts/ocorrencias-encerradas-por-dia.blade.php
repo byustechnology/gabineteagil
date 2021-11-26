3efa1fa5511026b763f3ac094f82b1cdcbccfe47
Pedro Roccon <pedro@pedroroccon.com.br>
2021-11-26 17:27:40 -0300

    Adicionando gráficos de ocorrências encerradas por dia

--- byustechnology/gabinete/src/views/dashboard/charts/ocorrencias-encerradas-por-dia.blade.php
+++ byustechnology/gabinete/src/views/dashboard/charts/ocorrencias-encerradas-por-dia.blade.php
@@ -1,27 +1,30 @@
+@php
+
+$faker = Faker\Factory::create();
+
+$ocorrencias = \ByusTechnology\Gabinete\Models\Ocorrencia::groupBy('dias')
+    ->select(app('db')->raw('DATE(created_at) AS dias'), app('db')->raw('count(*) as total'))
+    ->concluidas()
+    ->limit(15)
+    ->get()
+    ->each(function($item) {
+        $item->dias = date('d/m/Y', strtotime($item->dias));
+        $item->cor = 'rgba(255, 193, 7, 1)';
+    });
+@endphp
+
+@if ($ocorrencias->count())
 <canvas id="chartOcorrenciasEncerradasPorDia" width="200" height="200"></canvas>
 <script>
 var ctx = document.getElementById('chartOcorrenciasEncerradasPorDia').getContext('2d');
 var chartOcorrenciasEncerradasPorDia = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: [
-            '{{ today()->subDays(6)->format('d/m/Y') }}',
-            '{{ today()->subDays(5)->format('d/m/Y') }}',
-            '{{ today()->subDays(4)->format('d/m/Y') }}',
-            '{{ today()->subDays(3)->format('d/m/Y') }}',
-            '{{ today()->subDays(2)->format('d/m/Y') }}',
-            '{{ today()->subDays(1)->format('d/m/Y') }}'],
+        labels: <?php echo json_encode($ocorrencias->pluck('dias')->toArray()); ?>,
         datasets: [{
             label: 'Ocorrências',
-            data: [{{ mt_rand(1, 20) }}, {{ mt_rand(1, 20) }}, {{ mt_rand(1, 20) }}, {{ mt_rand(1, 20) }}, {{ mt_rand(1, 20) }}, {{ mt_rand(1, 20) }}],
-            backgroundColor: [
-                'rgba(255, 193, 7, 1)',
-                'rgba(255, 193, 7, 1)',
-                'rgba(255, 193, 7, 1)',
-                'rgba(255, 193, 7, 1)',
-                'rgba(255, 193, 7, 1)',
-                'rgba(255, 193, 7, 1)',
-            ],
+            data: <?php echo json_encode($ocorrencias->pluck('total')->toArray()); ?>,
+            backgroundColor: <?php echo json_encode($ocorrencias->pluck('cor')->toArray()); ?>,
             borderWidth: 1
         }]
     },
@@ -38,3 +41,8 @@ var chartOcorrenciasEncerradasPorDia = new Chart(ctx, {
     }
 });
 </script>
+@else
+    <div class="d-flex align-items-center justify-content-center text-center" style="height: 200px;">
+        <small class="text-muted"><i class="fas fa-exclamation-triangle fa-fw mr-1"></i> Não há dados para exibir. Conclua uma ocorrência para exibir o gráfico.</small>
+    </div>
+@endif
